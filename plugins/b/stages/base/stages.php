d2ce1554e634b0730d7fda5fb2cd9e1b61329595
christophe le drean <christophe@rouge-rose.net>
2011-09-07 22:38:11 +0200

    plugin stages (premiers fichiers)

---
+++ plugins/b/stages/base/stages.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function stages_declarer_champs_extras($champs = array()){
+	// prénom
+	$champs[] = new ChampExtra(array(
+		'table' => 'auteur',
+		'champ' => 'prenom',
+		'label' => 'stages:prenom',
+		'rechercher' => '',
+		'type' => 'ligne',
+		'sql' => "text NOT NULL DEFAULT ''",
+		'traitements' => ''
+	));
+
+	//activité
+	$champs[] = new ChampExtra(array(
+		'table' => 'auteur',
+		'champ' => 'activite',
+		'label' => 'stages:activite',
+		'rechercher' => '',
+		'type' => 'menu-enum',
+		'enum' => ",stages:type_defaut
+		1,stages:type_1
+		2,stages:type_2",
+		'sql' => "tinytext NOT NULL DEFAULT ''",
+		'traitements' => ''
+	));
+
+	return $champs;
+}
+?>
---
+++ plugins/b/stages/lang/act_fr.php
@@ -0,0 +1,10 @@
+<?php
+// Fichier ayant vocation à être surchargé depuis un autre plugin
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'label' => "Votre activit&eacute;",
+	// T
+	'type_defaut'=>"veuillez indiquer votre activit&eacute;",
+	'type_1'=>"Artiste",
+	'type_2'=>"&Eacute;tudiant-e"
+);
