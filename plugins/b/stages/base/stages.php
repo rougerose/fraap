b155fff977a499fc2e93f77766bd0d49ae472a63
christophe le drean <christophe@rouge-rose.net>
2011-09-10 11:38:34 +0200

    formulaire d'inscription via plugin inscription2

--- plugins/b/stages/base/stages.php
+++ plugins/b/stages/base/stages.php
@@ -2,17 +2,6 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function stages_declarer_champs_extras($champs = array()){
-	// prénom
-	$champs[] = new ChampExtra(array(
-		'table' => 'auteur',
-		'champ' => 'prenom',
-		'label' => 'stages:prenom',
-		'rechercher' => '',
-		'type' => 'ligne',
-		'sql' => "text NOT NULL DEFAULT ''",
-		'traitements' => ''
-	));
-
 	//activité
 	$champs[] = new ChampExtra(array(
 		'table' => 'auteur',
@@ -20,7 +9,7 @@ function stages_declarer_champs_extras($champs = array()){
 		'label' => 'stages:activite',
 		'rechercher' => '',
 		'type' => 'menu-enum',
-		'enum' => ",stages:type_defaut
+		'enum' => "0,stages:type_defaut
 		1,stages:type_1
 		2,stages:type_2",
 		'sql' => "tinytext NOT NULL DEFAULT ''",
--- plugins/b/stages/lang/stages_fr.php
+++ plugins/b/stages/lang/stages_fr.php
@@ -1,10 +1,22 @@
 <?php
 // Fichier ayant vocation à être surchargé depuis un autre plugin
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activite' => "Activit&eacute;",
+
 	// L
 	'label' => "Votre activit&eacute;",
+
+	// N
+	'nom' => "Votre nom",
+
+	// P
+	'prenom' => "Votre pr&eacute;nom",
+
+
 	// T
-	'type_defaut'=>"veuillez indiquer votre activit&eacute;",
+	'type_defaut'=>"quelle est votre activit&eacute; ?",
 	'type_1'=>"Artiste",
 	'type_2'=>"&Eacute;tudiant-e"
 );
