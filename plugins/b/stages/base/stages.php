988ef1aa7072d969be96a69ec05d4189297e5cce
christophe le drean <christophe@rouge-rose.net>
2011-09-13 02:05:04 +0200

    formulaire d'inscription spécifique sans passer par Inscriptions 2

--- plugins/b/stages/base/stages.php
+++ plugins/b/stages/base/stages.php
@@ -2,8 +2,17 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function stages_declarer_champs_extras($champs = array()){
-	// pour éviter que la colonne activite s'insère dans la table auteurs_elargis du plugin Inscriptions2
-	$exceptions_des_champs_auteurs_elargis = pipeline('i2_exceptions_des_champs_auteurs_elargis',array('activite'));
+	// prénom
+	$champs[] = new ChampExtra(array(
+		'table' => 'auteur',
+		'champ' => 'prenom',
+		'label' => 'stages:prenom',
+		'rechercher' => '',
+		'type' => 'ligne',
+		'sql' => "text NOT NULL DEFAULT ''",
+		'traitements' => ''
+	));
+
 	//activité
 	$champs[] = new ChampExtra(array(
 		'table' => 'auteur',
@@ -16,7 +25,7 @@ function stages_declarer_champs_extras($champs = array()){
 		2,stages:type_2",
 		'sql' => "tinytext NOT NULL DEFAULT ''",
 		'traitements' => ''
-	));
+		));
 
 	return $champs;
 }
--- plugins/b/stages/lang/stages_fr.php
+++ plugins/b/stages/lang/stages_fr.php
@@ -6,7 +6,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'activite' => "Activit&eacute;",
 
 	// L
-	'label' => "Votre activit&eacute;",
+	'label_activite' => "Votre activit&eacute;",
+	'label_conditions_inscription_stage' => "Oui, j'ai lu et j'accepte <a href='@url_conditions@'>les conditions d'utilisation</a>.",
+	'label_pass' => "Votre mot de passe",
+	'label_pass2'=> "Confirmez votre mot de passe",
+	'legende_identifiants' => "Vos identifiants",
+	'legende_identite' => "Votre identit&eacute;",
 
 	// N
 	'nom' => "Votre nom",
