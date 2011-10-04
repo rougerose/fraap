ec001db5aa25272611832d997f39dbf333299e8b
christophe le drean <christophe@rouge-rose.net>
2011-10-04 11:58:18 +0200

    tentative d'utiliser formitable, ça ressemble a un echec

--- plugins/b/stages/base/stages_install.php
+++ plugins/b/stages/base/stages_install.php
@@ -7,10 +7,44 @@ include_spip('base/stages');
 function stages_upgrade($nom_meta_base_version,$version_cible){
 	$champs = stages_declarer_champs_extras();
 	installer_champs_extras($champs, $nom_meta_base_version, $version_cible);
+
+	$version_actuelle = '0.0';
+	if (
+		(!isset($GLOBALS['meta'][$nom_meta_version_base]))
+		|| (($version_actuelle = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)
+	){
+
+		if (version_compare($version_actuelle,'0.0','=')){
+			// Création des tables
+			include_spip('base/create');
+			include_spip('base/abstract_sql');
+			creer_base();
+
+			echo "Installation du plugin Stages<br/>";
+			ecrire_meta($nom_meta_version_base, $version_actuelle=$version_cible, 'non');
+		}
+		/*
+		if (version_compare($version_actuelle,'0.5.7','<')){
+			include_spip('base/create');
+            maj_tables('spip_formulaires');
+
+			echo "Mise à jour du plugin formidable en version 0.5.7<br/>";
+			ecrire_meta($nom_meta_version_base, $version_actuelle=$version_cible, 'non');
+		}
+		*/
+	}
 }
 
 function stages_vider_tables($nom_meta_base_version) {
 	$champs = stages_declarer_champs_extras();
 	desinstaller_champs_extras($champs, $nom_meta_base_version);
+
+	include_spip('base/abstract_sql');
+
+	// On efface les tables du plugin
+	sql_drop_table('spip_stages');
+
+	// On efface la version entregistrée
+	effacer_meta($nom_meta_version_base);
 }
 ?>
---
+++ plugins/b/stages/base/stages_tables.php
@@ -0,0 +1,40 @@
+<?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function stages_declarer_tables_interfaces($interface){
+	// 'spip_' dans l'index de $tables_principales
+	$interface['table_des_tables']['stages'] = 'stages';
+	return $interface;
+}
+
+function stages_declarer_tables_principales($tables_principales){
+	//-- Table formulaires -----------------------------------------------------
+	$stages = array(
+		"id_formulaires_reponse" => "bigint(21) NOT NULL",
+		"regions" => "text NOT NULL",
+		"ville_stage" => "text NOT NULL",
+		"date_debut" => "text NOT NULL",
+		"date_fin" => "text NOT NULL",
+		"ecole" => "text NOT NULL",
+		"ville_ecole" => "text NOT NULL",
+		"diplome" => "text NOT NULL",
+		"niveau" => "text NOT NULL",
+		"competences" => "text NOT NULL",
+		"recherches" => "text NOT NULL",
+	);
+	$stages_cles = array(
+		"PRIMARY KEY" => "id_formulaires_reponse"
+	);
+	$tables_principales['spip_stages'] = array(
+		'field' => &$stages,
+		'key' => &$stages_cles,
+		'join'=> array(
+			'id_formulaires_reponse' => 'id_formulaires_response'
+		)
+	);
+
+	return $tables_principales;
+}
+?>
