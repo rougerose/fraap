ee00a45138f09b443a29adee92650a7dd101f67b
christophe le drean <christophe@rouge-rose.net>
2011-11-30 01:05:50 +0100

    formulaire candidature...

--- plugins/b/stages/formulaires/candidature.php
+++ plugins/b/stages/formulaires/candidature.php
@@ -2,6 +2,19 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+include_spip('inc/actions');
+include_spip('inc/editer');
 
+function formulaires_candidature_charger_dist($id_stage='new', $id_auteur='', $retour=''){
+	$valeurs = '';
+	return $valeurs;
+}
+function formulaires_candidature_verifier_dist($id_stage='new', $id_auteur='', $retour=''){
+//	$erreurs = formulaires_editer_objet_verifier('chat', $id_chat, array('nom'));
+	return $erreurs;
+}
+function formulaires_candidature_traiter_dist($id_stage='new', $id_auteur='', $retour=''){
+//	return formulaires_editer_objet_traiter('chat', $id_chat, '', '', $retour, '');
+}
 
 ?>
