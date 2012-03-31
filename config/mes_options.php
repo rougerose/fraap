78a0ac86dddf3b7d19458ef61eeb9bdb26afe087
christophe le drean <christophe@rouge-rose.net>
2012-03-31 10:47:09 +0200

    options

--- config/mes_options.php
+++ config/mes_options.php
@@ -7,15 +7,18 @@ define('_CACHE_CONTEXTES_AJAX',true);
 $GLOBALS['debut_intertitre']='<h2>';
 $GLOBALS['fin_intertitre']='</h2>';
 
-// augmenter la taille des logs
-// $GLOBALS['taille_des_logs'] = 5000;
-// afficher toutes les erreurs
-//error_reporting(E_ALL);
-//@ini_set("display_errors", "On");
-// afficher toutes les erreurs dans SPIP aussi
-// define('SPIP_ERREUR_REPORT',E_ALL);
-// afficher toutes les erreurs dans les pipelines aussi
-// define('SPIP_ERREUR_REPORT_INCLUDE_PLUGINS',E_ALL);
+$GLOBALS['dossier_squelettes']='2011';
+$GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+$GLOBALS['table_des_traitements']['NOM'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+$GLOBALS['toujours_paragrapher']=true;
+$GLOBALS['forcer_lang']=false;
+$GLOBALS['derniere_modif_invalide']=true;
+if (!defined('_DUREE_CACHE_DEFAUT')) define('_DUREE_CACHE_DEFAUT',86400);
+if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
+$GLOBALS['quota_cache']=10;
+//if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
+//if (!defined('_TRANCHES')) define('_TRANCHES',10);
+//if (!defined('_INTERDIRE_COMPACTE_HEAD_ECRIRE')) define('_INTERDIRE_COMPACTE_HEAD_ECRIRE',true);
 
 
 ?>
