330cc7e5d32f5d8d471cc3ba469ef0b7a85d08bc
christophe le drean <christophe@rouge-rose.net>
2012-03-31 13:23:10 +0200

    corrections
    - css
    - alternance des blocs de brèves
    - grille de mise en page
    - numéro de rubrique annuaire

--- 2011/javascript/public.js
+++ 2011/javascript/public.js
@@ -6,12 +6,12 @@ jQuery(document).ready(function() {
 	/*
 	 * Grille de mise en page ajoutée aux boutons d'administration de spip
 	 */
-/*	$("#spip-admin").append("<a id='grille' class='spip-admin-boutons' href='#'>Grille</a>");
+	$("#spip-admin").append("<a id='grille' class='spip-admin-boutons' href='#'>Grille</a>");
 	$("#grille").click(function(){
 		$("#page").toggleClass("gs");
 	});
 
-*/
+
 	/*
 	* jquery UI Tabs
 	*/
