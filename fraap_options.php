5fa092818a94b5af5fdf379ca89000b56a4b5591
christophe <christophe@rouge-rose.net>
2015-11-01 19:50:04 +0100

    squelettes sous forme de plugin

--- fraap_options.php
+++ fraap_options.php
@@ -1,13 +1,13 @@
 <?php
-define('_CACHE_CONTEXTES_AJAX',true);
+//define('_CACHE_CONTEXTES_AJAX',true);
 
-$GLOBALS['dossier_squelettes']='2011';
-$GLOBALS['debut_intertitre']='<h2>';
-$GLOBALS['fin_intertitre']='</h2>';
-$GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
-$GLOBALS['table_des_traitements']['NOM'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
-$GLOBALS['toujours_paragrapher']=true;
-$GLOBALS['forcer_lang']=false;
+// $GLOBALS['dossier_squelettes']='2011';
+// $GLOBALS['debut_intertitre']='<h2>';
+// $GLOBALS['fin_intertitre']='</h2>';
+// $GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+// $GLOBALS['table_des_traitements']['NOM'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+// $GLOBALS['toujours_paragrapher']=true;
+// $GLOBALS['forcer_lang']=false;
 // $GLOBALS['derniere_modif_invalide']=true;
 // if (!defined('_DUREE_CACHE_DEFAUT')) define('_DUREE_CACHE_DEFAUT',86400);
 // if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
