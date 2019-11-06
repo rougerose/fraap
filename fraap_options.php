55793de9d67b10f1557ccf3e6ec9cc7043f7e4f9
christophe le drean <christophe@rouge-rose.net>
2019-11-06 18:03:57 +0100

    Désactivé options dev de spip

--- fraap_options.php
+++ fraap_options.php
@@ -22,16 +22,16 @@ $GLOBALS['fin_intertitre'] = '</h2>';
 // if (!defined('_TRANCHES')) define('_TRANCHES',10);
 // if (!defined('_INTERDIRE_COMPACTE_HEAD_ECRIRE')) define('_INTERDIRE_COMPACTE_HEAD_ECRIRE',true);
 
-error_reporting(E_ALL^E_NOTICE);
-ini_set ("display_errors", "On");
-define('SPIP_ERREUR_REPORT',E_ALL);
-$GLOBALS['taille_des_logs'] = 50000;
-define('_MAX_LOG', 500000);
-define('_LOG_FILELINE',true);
-define('_LOG_FILTRE_GRAVITE',8);
-define('_DEBUG_AUTORISER', true);
-define('_DEBUG_SLOW_QUERIES', true);
-define('_BOUCLE_PROFILER', 5000);
+// error_reporting(E_ALL^E_NOTICE);
+// ini_set ("display_errors", "On");
+// define('SPIP_ERREUR_REPORT',E_ALL);
+// $GLOBALS['taille_des_logs'] = 50000;
+// define('_MAX_LOG', 500000);
+// define('_LOG_FILELINE',true);
+// define('_LOG_FILTRE_GRAVITE',8);
+// define('_DEBUG_AUTORISER', true);
+// define('_DEBUG_SLOW_QUERIES', true);
+// define('_BOUCLE_PROFILER', 5000);
 
 
 ?>
