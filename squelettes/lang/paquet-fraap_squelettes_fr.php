eb619d129b25db76fa60b3df4fbe42e97e4b3a52
christophe le drean <christophe@rouge-rose.net>
2021-12-30 11:27:09 +0100

    Plugins squelettes et thème

---
+++ squelettes/lang/paquet-fraap_squelettes_fr.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'fraap_squelettes_description' => 'Squelettes HTML du site',
+	'fraap_squelettes_slogan' => 'Squelettes HTML du site',
+);
