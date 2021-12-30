53c9d9434b913a793c7b6ba63c8127592fe6f489
christophe le drean <christophe@rouge-rose.net>
2021-12-30 11:25:47 +0100

    Plugin Fraap, version expérimentations:
    Premiers éléments de base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fraap.org
+
+Squelettes et thème du site [Fraap.org](https://fraap.org) avec [Spip](https://spip.net) v4.0 minimum.
---
+++ gulp.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  server: {
+    proxy: "http://localhost:8888/fraap.exp.dev",
+    notify: false,
+  },
+  scss: {
+    src: "theme/src/scss",
+    loadPath: ["node_modules"],
+  },
+  css: {
+    src: "theme/src/css",
+    dest: "theme/dist/css",
+  },
+  js: {
+    src: ["theme/src/js"],
+    dest: "theme/dist/js",
+    name: "fraap.js",
+  },
+  html: {
+    src: "squelettes/**/*.html",
+  },
+  clean: ["theme/dist/css/*.css", "theme/dist/js/*.js", "!theme/dist/"],
+  tasks: {
+    scss: true,
+    js: false,
+    clean: true,
+    reload: true,
+  },
+};
