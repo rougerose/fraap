d686df0b48662eb8b23cc1ede1f19b6f01a835c6
christophe le drean <christophe@rouge-rose.net>
2022-01-17 20:57:47 +0100

    skel (menus) : ajout menus entrée fraap_objet

---
+++ squelettes/lang/fraap_squelettes_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'lien_aller_au_contenu' => 'Aller au contenu',
+
+	// M
+	'menu_entree_afficher_icone' => "Afficher l'icone du menu déroulant (oui/non ; oui est la valeur par défaut)",
+	'menu_entree_descriptif' => "Une courte phrase de présentation de l'objet lorsqu'il s'agit d'un élément secteur",
+	'menu_objet_nom' => "Fraap : entrée d'un menu (objets : article ou rubrique)",
+	'menu_objet_description' => 'Ajouter une entrée du menu (entrée racine ou dans un menu déroulant).',
+);
