7e69de4d296d54759b333112d6ecb9b8469191fe
christophe le drean <christophe@rouge-rose.net>
2022-01-22 18:09:48 +0100

    Skel : squelettes de la navigation

--- squelettes/lang/fraap_squelettes_fr.php
+++ squelettes/lang/fraap_squelettes_fr.php
@@ -9,7 +9,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'lien_aller_au_contenu' => 'Aller au contenu',
 
 	// M
-	'menu_entree_afficher_icone' => "Afficher l'icone du menu déroulant (oui/non ; oui est la valeur par défaut)",
+	'menu_articles_rubrique_nom' => "Fraap : articles d'une rubrique",
+	'menu_entree_afficher_icone_md' => "Afficher l'icone du menu déroulant (oui/non ; non est la valeur par défaut).",
+	'menu_entree_afficher_icone_lien' => 'Afficher l\'icone ">" (oui/non ; oui est la valeur par défaut).',
 	'menu_entree_descriptif' => "Une courte phrase de présentation de l'objet lorsqu'il s'agit d'un élément secteur",
 	'menu_objet_nom' => "Fraap : entrée d'un menu (objets : article ou rubrique)",
 	'menu_objet_description' => 'Ajouter une entrée du menu (entrée racine ou dans un menu déroulant).',
