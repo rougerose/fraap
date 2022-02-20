e55002e3e682e3e88cc6c40d3be6df7b570c5f0e
christophe le drean <christophe@rouge-rose.net>
2022-02-20 16:14:52 +0100

    Fiches pratiques : affiner la démo
    Liste des mots-clés de niveau 2 :
    - Rendre apparent la hiérarchie niveau 1/niveau 2 des mots-clés
    à l'intérieur d'une branche, en ajoutant une page intermédiaire
    qui affiche la liste des mots-clés de niveau 2.
    Tous les mots-clés sont affichés, y compris ceux qui ne sont liés
    à aucun article. Le descriptif de chaque mot-clé est affiché,
    s'il existe.
    - Ajout, en bas de cette page supplémentaire, un encadré de mots-clés
    "à voir également" (sur le même sujet). La méthode de sélection de
    ces mots-clés suggérés est incertaine. À voir avec l'équipe si le
    niveau de pertinence des suggestions est suffisant ou s'il faut
    le modifier… ou envisager une liste alimentée "à la main".
    - Quelques améliorations d'ordre graphique. Rien de définitif,
    rien de fulgurant, mais tenter de mieux hiérarchiser.
    
    Liste des articles :
    - Page déjà présente et pour laquelle on ajoute essentiellement
    des améliorations d'ordre graphique.
    - Ici pas de suggestions "A voir également", car en l'état on ne voit
    pas très bien ce qui pourrait être pertinent.
    
    Page Article :
    - Quelques améliorations graphiques pour l'essentiel.
    
    Quelques notes sur les CSS :
    - Tentative de mieux utiliser la méthodologie CUBE mais ça reste très
    perfectible. Et il faudra virer ce qui a été fait précédemment.

--- squelettes/fraap_squelettes_options.php
+++ squelettes/fraap_squelettes_options.php
@@ -6,15 +6,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 // Zcore
 if (!isset($GLOBALS['z_blocs'])) {
-	$GLOBALS['z_blocs'] = array(
+	$GLOBALS['z_blocs'] = [
 		'content',
 		'head',
 		'head_js',
 		'header',
 		'footer',
-	);
+	];
 }
 
 // Premier niveau d'intertitre
 $GLOBALS['debut_intertitre'] = '<h2>';
 $GLOBALS['fin_intertitre'] = '</h2>';
+
+// Seuil à partir duquel la liste des mots-clés devient un formulaire de recherche
+define('_MAX_MOTS_LISTE', 150);
