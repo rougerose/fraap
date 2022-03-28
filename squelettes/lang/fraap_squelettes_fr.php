2d0e0cfbc5549a887c30cca28fc511be818db280
christophe le drean <christophe@rouge-rose.net>
2022-03-28 19:00:19 +0200

    Adaptation des saisies de menus

--- squelettes/lang/fraap_squelettes_fr.php
+++ squelettes/lang/fraap_squelettes_fr.php
@@ -4,17 +4,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
+$GLOBALS[$GLOBALS['idx_lang']] = [
 	// B
 	'bouton_valider' => 'Valider',
 	// L
 	'lien_aller_au_contenu' => 'Aller au contenu',
 
 	// M
-	'menu_articles_rubrique_nom' => "Fraap : articles d'une rubrique",
+	'menu_articles_rubrique_nom' => "Fraap : tous les articles d'une rubrique",
 	'menu_entree_afficher_icone_md' => "Afficher l'icone du menu déroulant (oui/non ; non est la valeur par défaut).",
 	'menu_entree_afficher_icone_lien' => 'Afficher l\'icone ">" (oui/non ; oui est la valeur par défaut).',
 	'menu_entree_descriptif' => "Une courte phrase de présentation de l'objet lorsqu'il s'agit d'un élément secteur",
 	'menu_objet_nom' => "Fraap : entrée d'un menu (objets : article ou rubrique)",
 	'menu_objet_description' => 'Ajouter une entrée du menu (entrée racine ou dans un menu déroulant).',
-);
+	'menu_entree_enfants' => 'Cet objet possède-t-il des enfants ? (oui/non ; non est la valeur par défaut).',
+];
