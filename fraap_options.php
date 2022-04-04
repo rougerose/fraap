37b0f03dbde5b9da8eb95a3192bd7e7f20057ae9
christophe le drean <christophe@rouge-rose.net>
2022-04-04 13:52:26 +0200

    Branche dev pour Nursit :
    - supprimer la totalité des squelettes de la version précédente
    - compatibilité du plugin pour spip 4.0 et 4.1

--- fraap_options.php
+++ fraap_options.php
@@ -4,5 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+define('_DEV_VERSION_SPIP_COMPAT', '4.0.99');
+
 $GLOBALS['debut_intertitre'] = '<h2 class="spip">';
 $GLOBALS['fin_intertitre'] = '</h2>';
