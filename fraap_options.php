ff7c6feaa87b3e0af60702a0571c740d52474330
christophe le drean <christophe@rouge-rose.net>
2022-04-20 17:34:36 +0200

    Remettre en place les squelettes du site, adaptées à Spip v4

--- fraap_options.php
+++ fraap_options.php
@@ -4,5 +4,20 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+// Zcore
+if (!isset($GLOBALS['z_blocs'])) {
+	$GLOBALS['z_blocs'] = [
+		'contenu',
+		'head',
+		'head_js',
+		'header',
+		'footer',
+		'titre',
+		'navigation',
+		'extra',
+		'fil',
+	];
+}
+
 $GLOBALS['debut_intertitre'] = '<h2 class="spip">';
 $GLOBALS['fin_intertitre'] = '</h2>';
