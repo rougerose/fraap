5a2f2f01643b2e0f4ccdfa122c4f3333430902a0
christophe le drean <christophe@rouge-rose.net>
2022-05-26 12:41:37 +0200

    Gulpfile : debug du reload avec browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const size = require("gulp-size");
 const rename = require("gulp-rename");
-const rollup = require("./rollup-task");
+const rollup = require("rollup");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const { terser } = require("rollup-plugin-terser");
 const commonjs = require("@rollup/plugin-commonjs");
@@ -13,33 +13,42 @@ const commonjs = require("@rollup/plugin-commonjs");
 // CONFIG
 const config = {
   server: {
-    proxy: "http://localhost:8888/fraap.exp.dev",
+    proxy: "http://localhost:8888/fraap.exp.dev/",
   },
   scss: {
-    src: "theme/src/scss",
+    src: "theme/src/scss/",
+    dest: "theme/dist/css/",
     loadPath: ["node_modules"],
   },
   css: {
     src: "theme/src/css",
     dest: "theme/dist/css",
   },
-  js: [
-    {
-      input: "theme/src/js/index.js",
-      output: "theme/dist/js/fraap.js",
-      format: "iife",
-    },
-    {
-      input: "focus-visible/src/focus-visible.js",
-      output: "theme/dist/js/polyfill/focus-visible.js",
-      format: "umd",
-    },
-  ],
+  js: {
+    src: "theme/src/js/",
+    dest: "theme/dist/js/",
+    name: "fraap.js",
+    modules: [
+      {
+        input: "theme/src/js/index.js",
+        output: "theme/dist/js/fraap.js",
+        format: "iife",
+      },
+      {
+        input: "focus-visible/src/focus-visible.js",
+        output: "theme/dist/js/polyfill/focus-visible.js",
+        format: "umd",
+      },
+    ],
+  },
+  copy: {
+    src: ["node_modules/focus-visible/dist/focus-visible.min.js"],
+    dest: "theme/dist/js/polyfill/",
+  },
   html: { src: "squelettes/**/*.html" },
   clean: [
     "theme/dist/css/*.css",
     "theme/dist/js/*.js",
-    "theme/dist/js/polyfill/*.js",
     "!theme/dist/",
   ],
   tasks: {
@@ -47,6 +56,7 @@ const config = {
     js: true,
     clean: true,
     reload: true,
+    copy: true,
   },
 };
 
@@ -62,13 +72,12 @@ const clean = function (done) {
 // SCSS task
 const scss = function (done) {
   if (!config.tasks.scss) return done();
-  return src(config.scss.src + "/index.scss")
+  return src(config.scss.src + "**/*.scss")
     .pipe(
       sass
         .sync({ includePaths: config.scss.loadPath })
         .on("error", sass.logError)
     )
-    // .pipe(dest(config.css.src))
     .pipe(postcss())
     .pipe(size({ title: "CSS", gzip: true, showFiles: true }))
     .pipe(rename({ basename: "fraap" }))
@@ -77,10 +86,25 @@ const scss = function (done) {
 };
 
 // JS task
-// Créer de multiples bundles js
-// Source : https://gist.github.com/craigdmckenna/c420f58bf4fdb35ac8923365555f1fb0
-const js = function (cb) {
-  rollup(config.js, cb);
+const js = function (done) {
+  if (!config.tasks.js) return done();
+  return rollup.rollup({
+    input: config.js.src + "/index.js",
+    plugins: [nodeResolve(), commonjs()],
+  }).then(bundle => {
+    return bundle.write({
+      file: config.js.dest + config.js.name,
+      format: "iife",
+      plugins: [process.env.NODE_ENV === "production" && terser()]
+    });
+  });
+}
+
+// Copy task
+const copy = (done) => {
+  if (!config.tasks.copy) return done();
+  return src(config.copy.src)
+    .pipe(dest(config.copy.dest));
 }
 
 // Server task
@@ -88,7 +112,7 @@ const startServer = function (done) {
   if (!config.tasks.reload) return done();
   // Initialize BrowserSync
   browserSync.init({
-    proxy: config.server.proxy,
+    proxy: config.server.proxy
   });
   done();
 };
@@ -102,19 +126,17 @@ const reloadBrowser = function (done) {
 
 // Watch
 const watchSource = function (done) {
-  watch(config.scss.src + "/**/*.scss", series(scss, reloadBrowser));
-  watch(config.js.src + "/**/*.js", series(js, reloadBrowser));
-  watch(config.html.src, series(scss, js, reloadBrowser));
+  watch(config.scss.src + "**/*.scss", series(scss));
+  watch(config.js.src + "**/*.js", series(js, reloadBrowser));
+  watch(config.html.src, series(reloadBrowser));
   done();
 };
 
-
 // Export tasks
-// exports.default = series(clean, parallel(scss), startServer, watchSource);
 exports.default = series(clean, parallel(scss, js));
-exports.dev = series(clean, parallel(scss, js));
 exports.build = series(clean, parallel(scss, js));
 exports.server = series(clean, parallel(scss, js), startServer, watchSource);
 exports.clean = clean;
 exports.js = js;
+exports.copy = copy;
 exports.scss = series(clean, scss);
