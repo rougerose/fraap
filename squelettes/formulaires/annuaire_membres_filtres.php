9a46a3c98f693b85955a9a4d3b3e8282d1179822
christophe le drean <christophe@rouge-rose.net>
2022-09-07 16:17:04 +0200

    Update: annuaire membres
    - ajout de l'id_rubrique dans les paramètres du formulaire
    - recharger la page avec ajax

--- squelettes/formulaires/annuaire_membres_filtres.php
+++ squelettes/formulaires/annuaire_membres_filtres.php
@@ -4,20 +4,20 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-function formulaires_annuaire_membres_filtres_charger($articles = [], $redirect = null) {
+function formulaires_annuaire_membres_filtres_charger($id_rubrique = null, $redirect = null) {
 	$valeurs = [];
-	$valeurs['articles'] = $articles;
+	$valeurs['id_rubrique'] = $id_rubrique;
 	$valeurs['btnOpen'] = _request('btnOpen');
 	$valeurs['mots'] = _request('mots');
 	return $valeurs;
 }
 
-function formulaires_annuaire_membres_filtres_verifier($articles = [], $redirect = null) {
+function formulaires_annuaire_membres_filtres_verifier($id_rubrique = null, $redirect = null) {
 	$erreurs = [];
 	return $erreurs;
 }
 
-function formulaires_annuaire_membres_filtres_traiter($articles = [], $redirect = null) {
+function formulaires_annuaire_membres_filtres_traiter($id_rubrique = null, $redirect = null) {
 	// refuser_traiter_formulaire_ajax();
 	$retour = [];
 	$mots = _request('mots');
@@ -31,6 +31,7 @@ function formulaires_annuaire_membres_filtres_traiter($articles = [], $redirect 
 		}
 		$redirect = ancre_url($redirect, 'annuaire');
 		$retour['redirect'] = $redirect;
+		$retour['message_ok'] = '<script type="text/javascript">if (window.jQuery) ajaxReload("articles");</script>';
 	}
 
 	$retour['editable'] = true;
