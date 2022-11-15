c66d5d760df977e52d25ee5bddb71cb59fe31a9b
christophe le drean <christophe@rouge-rose.net>
2022-11-15 16:33:52 +0100

    Fichiers css et js compressés pour test

--- theme/dist/js/fraap-dialog-filtres-init.js
+++ theme/dist/js/fraap-dialog-filtres-init.js
@@ -1,12 +1,1 @@
-(function () {
-	'use strict';
-
-	// Dialog init
-	let dialogEl = document.querySelector("#dialogFiltres"),
-	options = { allowTouchMove: () => true };
-
-	if (dialogEl) {
-	new Fraap.FraapDialog(dialogEl, options);
-	}
-
-})();
+!function(){"use strict";let e=document.querySelector("#dialogFiltres"),o={allowTouchMove:()=>!0};e&&new Fraap.FraapDialog(e,o)}();
