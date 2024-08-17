c97e60cf269a9c26002ad7087dcb8de534fa3b66
christophe le drean <christophe@rouge-rose.net>
2024-08-17 15:08:45 +0200

    feat (territoires) : script de migration des mots-clés vers territoires

---
+++ squelettes/formulaires/migrer_territoires.php
@@ -0,0 +1,171 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('action/editer_liens');
+
+function formulaires_migrer_territoires_charger($data = []) {
+	$valeurs = [
+		'data' => $data,
+	];
+	return $valeurs;
+}
+
+function formulaires_migrer_territoires_verifier($data = []) {
+	$erreurs = [];
+
+	if (count($data['art_mots_departements']) == 0 and count($data['art_mots_regions']) == 0) {
+		$erreurs['message_erreur'] = 'Aucun mot-clé Départements et Régions à migrer';
+	}
+
+	$where_solde_departements = ['statut = ' . sql_quote('publie'), 'id_rubrique = ' . sql_quote('3'), sql_in('id_article', $data['art_mots_departements']), sql_in('id_article', $data['art_departements'], 'NOT')];
+
+	$solde_departements = get_solde($where_solde_departements);
+
+	$where_solde_regions = ['statut = ' . sql_quote('publie'), 'id_rubrique = ' . sql_quote('3'), sql_in('id_article', $data['art_mots_regions']), sql_in('id_article', $data['art_regions'], 'NOT')];
+
+	$solde_regions = get_solde($where_solde_regions);
+
+	if (!count($erreurs) and !_request('confirm')) {
+		$message = '<strong>Résultats de la prévisualisation</strong><br>';
+		$message .= sinon(
+			singulier_ou_pluriel(
+				count($solde_departements),
+				'fraap_squelettes:migration_info_preview_departements_singulier',
+				'fraap_squelettes:migration_info_preview_departements_pluriel'
+			),
+			_T('fraap_squelettes:migration_info_departements_aucun')
+		) . '<br>';
+		$message .= sinon(
+			singulier_ou_pluriel(
+				count($solde_regions),
+				'fraap_squelettes:migration_info_preview_regions_singulier',
+				'fraap_squelettes:migration_info_preview_regions_pluriel'
+			),
+			_T('fraap_squelettes:migration_info_regions_aucun')
+		) . '<br>';
+
+		$erreurs['confirmer'] = $message;
+		$erreurs['message_erreur'] = '';
+	}
+	return $erreurs;
+}
+
+function formulaires_migrer_territoires_traiter($data = []) {
+	$res = [];
+
+	// Récupérer les correspondances id_mot -> id_territoire depuis un fichier json
+	$json = json_decode(file_get_contents(find_in_path('json/data-mots-territoires.json')), true);
+
+	if ($json === false) {
+		return $res['message_erreur'] = 'Le tableau de correspondances mots-clé/territoires est manquant.';
+	} else {
+		$correspondances = [];
+		foreach ($json as $key => $value) {
+			if ($value['id_mot']) {
+				$correspondances[$value['id_mot']] = $value['id_territoire'];
+			}
+		}
+	}
+
+	// Récupérer les articles restants à migrer.
+	$where_solde_departements = ['statut = ' . sql_quote('publie'), 'id_rubrique = ' . sql_quote('3'), sql_in('id_article', $data['art_mots_departements']), sql_in('id_article', $data['art_departements'], 'NOT')];
+
+	$solde_departements = get_solde($where_solde_departements);
+
+	$where_solde_regions = ['statut = ' . sql_quote('publie'), 'id_rubrique = ' . sql_quote('3'), sql_in('id_article', $data['art_mots_regions']), sql_in('id_article', $data['art_regions'], 'NOT')];
+
+	$solde_regions = get_solde($where_solde_regions);
+
+	// Récupérer Départements (id_article et id_mot) et associer avec le territoire équivalent
+	$from = 'spip_articles AS articles INNER JOIN spip_mots_liens AS L1 ON L1.id_objet = articles.id_article AND L1.objet=' . sql_quote('article') . ' INNER JOIN spip_mots AS L2 ON L2.id_mot = L1.id_mot';
+
+	$where = [
+		'articles.statut = ' . sql_quote('publie'),
+		'articles.id_rubrique = ' . intval('3'),
+		sql_in('articles.id_article', $solde_departements),
+		'L1.objet = ' . sql_quote('article'),
+		'L2.id_groupe = ' . intval('10'),
+	];
+
+	$article_departements = sql_allfetsel('articles.id_article, L2.id_mot', $from, $where);
+
+	$nb_departements = 0;
+
+	if (count($article_departements)) {
+		foreach ($article_departements as $article) {
+			$id_mot = $article['id_mot'];
+			$id_territoire = $correspondances[$id_mot];
+			if ($id_territoire) {
+				objet_associer(
+					['territoire' => $id_territoire],
+					['article' => $article['id_article']]
+				);
+				$nb_departements++;
+			}
+		}
+	}
+
+	// idem pour les Régions
+	$where = [
+		'articles.statut = ' . sql_quote('publie'),
+		'articles.id_rubrique = ' . intval('3'),
+		sql_in('articles.id_article', $solde_regions),
+		'L1.objet = ' . sql_quote('article'),
+		'L2.id_groupe = ' . intval('1'),
+	];
+	$article_regions = sql_allfetsel('articles.id_article, L2.id_mot', $from, $where);
+
+	$nb_regions = 0;
+
+	if (count($article_regions)) {
+		foreach ($article_regions as $article) {
+			$id_mot = $article['id_mot'];
+			$id_territoire = $correspondances[$id_mot];
+			if ($id_territoire) {
+				objet_associer(
+					['territoire' => $id_territoire],
+					['article' => $article['id_article']]
+				);
+				$nb_regions++;
+			}
+		}
+	}
+
+	$message = '<strong>Résultats de la migration</strong><br>';
+	$message .= sinon(
+		singulier_ou_pluriel(
+			$nb_departements,
+			'fraap_squelettes:migration_info_departements_singulier',
+			'fraap_squelettes:migration_info_departements_pluriel'
+		),
+		_T('fraap_squelettes:migration_info_departements_aucun')
+	) . '<br>';
+	$message .= sinon(
+		singulier_ou_pluriel(
+			count($solde_regions),
+			'fraap_squelettes:migration_info_regions_singulier',
+			'fraap_squelettes:migration_info_regions_pluriel'
+		),
+		_T('fraap_squelettes:migration_info_regions_aucun')
+	) . '<br>';
+
+	$res['message_ok'] = $message;
+
+	return $res;
+}
+
+
+function get_solde($where) {
+	$solde = [];
+
+	if ($ids = sql_allfetsel('id_article', 'spip_articles', $where)) {
+		foreach ($ids as $id) {
+			$solde[] = $id['id_article'];
+		}
+	}
+
+	return $solde;
+}
